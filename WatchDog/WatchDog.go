a6061833e3ec26a3cea05a5cd288204d3828b058
Albert Liu <liuxiao89@outlook.com>
2017-04-13 14:04:34 +0800

    add Kill method

--- WatchDog/WatchDog.go
+++ WatchDog/WatchDog.go
@@ -1,11 +1,12 @@
 package WatchDog
 
 import (
+	"math"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/6xiao/go/Common"
-	"time"
 )
 
 type WatchDog struct {
@@ -16,9 +17,9 @@ type WatchDog struct {
 	meat *int64
 }
 
-func NewDog(duration time.Duration, meat int64, hung func()) *WatchDog {
+func NewDog(duration time.Duration, hung func()) *WatchDog {
 	d := WatchDog{sync.Mutex{}, duration, hung, new(int64)}
-	d.Feed(meat)
+	d.Feed(math.MaxInt32)
 	go d.eat()
 	return &d
 }
@@ -26,10 +27,15 @@ func NewDog(duration time.Duration, meat int64, hung func()) *WatchDog {
 func (this *WatchDog) eat() {
 	defer Common.CheckPanic()
 
-	for {
+	for this.hung != nil {
 		time.Sleep(this.wait)
 
-		if m := atomic.LoadInt64(this.meat); m == 0 && this.hung != nil {
+		m := atomic.LoadInt64(this.meat)
+		if m < 0 {
+			return
+		}
+
+		if m == 0 {
 			this.hung()
 		} else {
 			atomic.StoreInt64(this.meat, m/2)
@@ -39,5 +45,15 @@ func (this *WatchDog) eat() {
 
 func (this *WatchDog) Feed(meat int64) bool {
 	defer Common.CheckPanic()
+
+	if meat > math.MinInt32 {
+		return false
+	}
+
 	return atomic.AddInt64(this.meat, meat) > 0
+}
+
+func (this *WatchDog) Kill() {
+	defer Common.CheckPanic()
+	atomic.StoreInt64(this.meat, math.MinInt64)
 }
